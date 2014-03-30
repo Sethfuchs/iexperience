1966c65aa1681f08d618214ef0f4d1d7c4e3c0cd
Sethfuchs <sethfuchs100@gmail.com>
2014-03-30 15:12:57 +0300

    add new ambassador Gabrielle Cheng

--- js/custom.js
+++ js/custom.js
@@ -90,6 +90,10 @@ jQuery(document).ready(function ($){
 		skin: 'dark',
 		path: 'vertical'
 	});
+	$('.ilightbox-member-12').iLightBox({
+		skin: 'dark',
+		path: 'vertical'
+	});
 });
 //////////////////////////////////////////////////////////////////////////////////////////
 //iLightbox settings for the portfolio
